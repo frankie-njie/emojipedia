236aa6f57f5096d5b3323f55ce47acc7a4afc596
frankie <frankie@Frankie-MacBook-Pro.local>
2021-06-17 17:46:13 +0100

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,101 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
+  },
+  {
+    id: 5,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+  },
+  {
+    id: 6,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning: 
+      "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude." 
+  },
+  {
+    id: 7,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning: 
+     "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo." 
+  },
+  {
+    id: 8,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning: "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness. " 
+  },
+  {
+    id: 9,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning: "The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea." 
+  },
+  {
+    id: 10,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning: "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015." 
+  },
+  {
+    id: 11,
+    emoji: "👌",
+    name: "Ok Hand Sign",
+    meaning: "Thumb and forefinger form an O. Symbol for “Okay!“ Sign of approval, agreement or that everything is okay. Careful, in some countries it is considered offensive and insulting" 
+  },
+  {
+    id: 12,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning: "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach" 
+  },
+  {
+    id: 13,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning: "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around. " 
+  },
+  {
+    id: 14,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning: "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck." 
+  },
+  {
+    id: 15,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning: "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval." 
+  }
+];
+
+export default emojipedia;
